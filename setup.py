54b91873f3b6fb2ef0d9d36f4479e28b071bf46e
João M. Bezerra <marcospb19@hotmail.com>
2023-03-12 16:28:42 -0300

    push setup-not-working to share code
    with doomsday

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 from hhat_lang import version
 
-
 readme = open("README.md", "r").read()
 
 requirements = open("requirements.txt", "r").readlines()
 
 setup(
-    author="Eduardo Maschio",
-    python_requires=">=3.8",
+    name="H-hat",
+    version='0.1.0',
     description="H-hat: a high level quantum programming language.",
     long_description=readme,
-    include_package_data=True,
-    name="H-hat",
+    author="Eduardo Maschio",
     packages=find_packages(include=["hhat_lang", "hhat_lang.*"]),
-    requires=requirements,
-    version=version
+    install_requires=requirements,
+    python_requires="==3.10.9",
+    include_package_data=True,
 )

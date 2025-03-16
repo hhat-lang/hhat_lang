0ae5e1b64d96df610a65ee6d77e8ed1c33dfaebf
Doomsk <eduardo.maschio@theqube.io>
2025-03-16 19:31:50 +0100

    add fresh new structure

---
+++ rust/hhat_lang/src/hhat_core/src/lib.rs
@@ -0,0 +1,24 @@
+#![allow(dead_code, unused_attributes, unused_attributes, unused_variables)]
+mod ast;
+mod compiler;
+mod data_traits;
+mod instr;
+mod ir;
+mod ira_impls;
+mod memory;
+mod utils;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}

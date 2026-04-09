dc5a4110446c83f4f4ef3207cfa813457d2a1b38
Doomsk <eduardo.maschio@hhat-lang.org>
2026-04-09 22:59:14 +0200

    fix code blocks white stripes and missing logo
    Signed-off-by: Doomsk <eduardo.maschio@theqube.io>

--- python/src/hhat_lang/dialects/heather/toolchain/pygments/mkdocs_plugin.py
+++ python/src/hhat_lang/dialects/heather/toolchain/pygments/mkdocs_plugin.py
@@ -12,8 +12,7 @@ class HhatCssPlugin(BasePlugin):
         ("light_style", c.Type(str, default="hhat_heather_light")),
         ("dark_style", c.Type(str, default="hhat_heather_dark")),
         ("light_scheme", c.Type(str, default="default")),  # Material light scheme name
-        ("dark_scheme", c.Type(str, default="slate")),     # Material dark scheme name
-        ("css_path", c.Type(str, default="stylesheets/hhat-heather-pygments.css")),
+        ("dark_scheme", c.Type(str, default="slate")),  # Material dark scheme name
         ("selector", c.Type(str, default=".highlight")),
     )
 
@@ -24,15 +23,15 @@ class HhatCssPlugin(BasePlugin):
 
         # Scope each generated stylesheet to Material's scheme attribute selector:
         light_sel = f'[data-md-color-scheme="{self.config["light_scheme"]}"] {self.config["selector"]}'
-        dark_sel  = f'[data-md-color-scheme="{self.config["dark_scheme"]}"] {self.config["selector"]}'
+        dark_sel = f'[data-md-color-scheme="{self.config["dark_scheme"]}"] {self.config["selector"]}'
 
-        light_css = HtmlFormatter(style=self.config["light_style"]).get_style_defs(light_sel)
-        dark_css  = HtmlFormatter(style=self.config["dark_style"]).get_style_defs(dark_sel)
+        light_css = HtmlFormatter(style=self.config["light_style"]).get_style_defs(
+            light_sel
+        )
+        dark_css = HtmlFormatter(style=self.config["dark_style"]).get_style_defs(
+            dark_sel
+        )
 
         out_path.write_text(light_css + "\n\n" + dark_css + "\n", encoding="utf-8")
-
-        extra_css = config.setdefault("extra_css", [])
-        if self.config["css_path"] not in extra_css:
-            extra_css.append(self.config["css_path"])
 
         return config

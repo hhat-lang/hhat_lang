8c862c03774830a9cdf3ba9a8100c6d0f9d6bcd6
baihaoyu11 <baihaoyu15@gmail.com>
2026-06-10 23:50:21 +1000

    Align main documentation filename

--- python/src/hhat_lang/toolchain/project/__init__.py
+++ python/src/hhat_lang/toolchain/project/__init__.py
@@ -11,7 +11,7 @@ PROOFS_FOLDER_NAME = "proofs"  # future use
 # files
 MAIN_FILE_NAME = "main.hat"
 
-MAIN_DOC_FILE_NAME = f"{MAIN_FILE_NAME}.md"
+MAIN_DOC_FILE_NAME = "main.md"
 
 # paths
 MAIN_PATH = f"{SOURCE_FOLDER_NAME}/{MAIN_DOC_FILE_NAME}"
